fbc4779b0b6633f3f3508ee924f25dc0800f4f63
Sebastien Carceles <sebastien.carceles@gmail.com>
2017-10-15 14:02:52 +0200

    try headless chrome

--- app/business/linkedin_scrapper.rb
+++ app/business/linkedin_scrapper.rb
@@ -45,29 +45,29 @@ class LinkedinScrapper
   end
 
   def build_session
-    # chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
-    # chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
-    # Capybara.register_driver :chrome do |app|
-    #   Capybara::Selenium::Driver.new(
-    #      app,
-    #      browser: :chrome,
-    #      desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
-    #   )
+    chrome_bin = ENV.fetch('GOOGLE_CHROME_SHIM', nil)
+    chrome_opts = chrome_bin ? { "chromeOptions" => { "binary" => chrome_bin } } : {}
+    Capybara.register_driver :chrome do |app|
+      Capybara::Selenium::Driver.new(
+         app,
+         browser: :chrome,
+         desired_capabilities: Selenium::WebDriver::Remote::Capabilities.chrome(chrome_opts)
+      )
+    end
+    Capybara::Session.new(:chrome)
+
+
+    # Capybara.register_driver :selenium do |app|
+    #   Capybara::Selenium::Driver.new(app, browser: :chrome)
     # end
-
-
-    Capybara.register_driver :selenium do |app|
-      Capybara::Selenium::Driver.new(app, browser: :chrome)
-    end
-    Capybara.javascript_driver = :chrome
-    Capybara.configure do |config|
-      config.default_max_wait_time = 10 # seconds
-      config.default_driver = :selenium
-    end
-
-    session = Capybara::Session.new(:selenium)
-    session.driver.browser.manage.window.resize_to(2_500, 2_500)
-    session
+    # Capybara.javascript_driver = :chrome
+    # Capybara.configure do |config|
+    #   config.default_max_wait_time = 10 # seconds
+    #   config.default_driver = :selenium
+    # end
+    # session = Capybara::Session.new(:selenium)
+    # session.driver.browser.manage.window.resize_to(2_500, 2_500)
+    # session
 
 
     # Capybara.register_driver :chrome do |app|
