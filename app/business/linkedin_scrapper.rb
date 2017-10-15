1a25ab336cf0caeb4da39a80620b0b6337237a73
Sebastien Carceles <sebastien.carceles@gmail.com>
2017-10-15 11:45:36 +0200

    try headless chrome

--- app/business/linkedin_scrapper.rb
+++ app/business/linkedin_scrapper.rb
@@ -71,22 +71,22 @@ class LinkedinScrapper
     # session
 
 
-    
-    
+
+
     Capybara.register_driver :chrome do |app|
       Capybara::Selenium::Driver.new(app, browser: :chrome)
     end
-    
+
     Capybara.register_driver :headless_chrome do |app|
       capabilities = Selenium::WebDriver::Remote::Capabilities.chrome(
         chromeOptions: { args: %w(headless disable-gpu) }
       )
-    
+
       Capybara::Selenium::Driver.new app,
         browser: :chrome,
         desired_capabilities: capabilities
     end
-    
+
     Capybara.javascript_driver = :headless_chrome
 
     Capybara::Session.new(:headless_chrome)
