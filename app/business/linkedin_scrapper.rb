ef83e005ff6f83008d2e5ca182db4f368e8ce268
Sebastien Carceles <sebastien.carceles@gmail.com>
2017-10-15 10:57:24 +0200

    push

--- app/business/linkedin_scrapper.rb
+++ app/business/linkedin_scrapper.rb
@@ -47,6 +47,7 @@ class LinkedinScrapper
     Capybara.register_driver :selenium do |app|
       Capybara::Selenium::Driver.new(app, browser: :chrome)
     end
+    
     Capybara.javascript_driver = :chrome
     Capybara.configure do |config|
       config.default_max_wait_time = 10 # seconds
