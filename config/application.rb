206b2654b303f7bedd9b78b2b20bf1274d6cff71
Sebastien Carceles <sebastien.carceles@gmail.com>
2017-12-08 10:12:06 +0100

    give it a try without the problematic asset

--- config/application.rb
+++ config/application.rb
@@ -15,9 +15,6 @@ module CompanyIo
       g.fixture_replacement :factory_girl
     end
 
-    # Serve assests
-    config.serve_static_assets = true
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
