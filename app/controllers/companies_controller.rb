95738c23192894f787b6d000abe00b5400bd9bac
Sebastien Carceles <sebastien.carceles@gmail.com>
2017-12-10 09:42:46 +0100

    find company

--- app/controllers/companies_controller.rb
+++ app/controllers/companies_controller.rb
@@ -1,11 +1,22 @@
 # frozen_string_literal: true
 
 class CompaniesController < ApplicationController
+  before_action :find_company, only: :show
+
   def search
     query = params[:q]
     @companies = Company.fuzzy_search(name: query)
   end
 
-  def show
+  def show  
   end
+
+  private
+
+    def find_company
+      @company = Company.find_by_id(params[:id])
+      @company ||= Company.find_by_slug(params[:id])
+      @company ||= Company.find_by_name(params[:id])
+      raise ActiveRecord::RecordNotFound.new("#{params[:id]} not found") unless @company
+    end
 end
