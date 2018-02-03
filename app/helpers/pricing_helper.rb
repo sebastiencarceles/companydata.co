79eae7c92a897f4a1011414d126f76df0d3b4fbb
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-02-03 17:13:14 +0100

    choose the plan

---
+++ app/helpers/pricing_helper.rb
@@ -0,0 +1,9 @@
+module PricingHelper
+  def plan_higher?(plan_a, plan_b)
+    User::PLANS.keys.index(plan_a.to_sym) > User::PLANS.keys.index(plan_b.to_sym)
+  end
+
+  def plan_lower?(plan_a, plan_b)
+    User::PLANS.keys.index(plan_a.to_sym) < User::PLANS.keys.index(plan_b.to_sym)
+  end
+end
