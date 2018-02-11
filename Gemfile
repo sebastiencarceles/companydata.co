6964c17aa6c902aa6232883840065323258b9e29
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-02-11 18:51:01 +0100

    sendgrid integration

--- Gemfile
+++ Gemfile
@@ -62,6 +62,9 @@ gem "activerecord-import"
 # Forms
 gem "simple_form"
 
+# Emails
+gem "sendgrid-ruby"
+
 group :development, :test do
   # Call 'byebug' anywhere in the code to stop execution and get a debugger console
   gem "byebug", platforms: [:mri, :mingw, :x64_mingw]
--- config/environment.rb
+++ config/environment.rb
@@ -1,5 +1,17 @@
+# frozen_string_literal: true
+
 # Load the Rails application.
-require_relative 'application'
+require_relative "application"
 
 # Initialize the Rails application.
 Rails.application.initialize!
+
+ActionMailer::Base.smtp_settings = {
+  user_name: Figaro.env.SENDGRID_USERNAME,
+  password: Figaro.env.SENDGRID_PASSWORD,
+  domain: "companydata.co",
+  address: "smtp.sendgrid.net",
+  port: 587,
+  authentication: :plain,
+  enable_starttls_auto: true
+}
