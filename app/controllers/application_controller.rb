d2b99471ad6763c346275eb6c7774cc8b4b14dfd
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-02-12 21:48:30 +0100

    internationalize

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -4,4 +4,18 @@ class ApplicationController < ActionController::Base
   layout "_base"
 
   protect_from_forgery with: :exception
+
+  before_action :set_locale
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
+
+  private
+
+    def set_locale
+      locale = params[:locale] || session[:locale] || I18n.default_locale
+      I18n.locale = locale
+      session[:locale] = locale
+    end
 end
