74695404bdeeac2df3e16c0750c7d04c5ce6cfa8
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-02-18 23:43:12 +0100

    fix offenses

--- app/serializers/api/v1/unauth_company_serializer.rb
+++ app/serializers/api/v1/unauth_company_serializer.rb
@@ -8,23 +8,23 @@ class Api::V1::UnauthCompanySerializer < ActiveModel::Serializer
     :website_url,
     :api_url
 
-    def id
-      return object.id
-    end
+  def id
+    return object.id
+  end
 
-    def name
-      return object.name
-    end
+  def name
+    return object.name
+  end
 
-    def smooth_name
-      return object.smooth_name
-    end
+  def smooth_name
+    return object.smooth_name
+  end
 
-    def website_url
-      "https://www.companydata.co/companies/#{object.slug}"
-    end
+  def website_url
+    "https://www.companydata.co/companies/#{object.slug}"
+  end
 
-    def api_url
-      "https://www.companydata.co/api/v1/companies/#{object.slug}"
-    end
+  def api_url
+    "https://www.companydata.co/api/v1/companies/#{object.slug}"
+  end
 end
