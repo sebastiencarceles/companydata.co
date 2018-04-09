29bf4de7ab8dd889ed7379e0c07f1177ea0cc38d
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-04-09 18:24:11 +0200

    add a controller to manage slack commands

---
+++ app/controllers/commands_controller.rb
@@ -0,0 +1,17 @@
+class CommandsController < ApplicationController
+  def create
+    return render json: {}, status: 403 unless valid_slack_token?
+    
+    render json: { response_type: "ephemeral" }, status: :created
+  end
+
+  private
+
+    def valid_slack_token?
+      params[:token] == Figaro.env.SLACK_SLASH_COMMAND_TOKEN
+    end
+
+    def command_params
+      params.permit(:text, :token, :user_id, :response_url)
+    end
+end
