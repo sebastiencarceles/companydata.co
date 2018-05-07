5d2674395889beda08bd366fbd2a54c370b6da60
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-05-07 15:17:09 +0200

    remove useless table codes

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180507092852) do
+ActiveRecord::Schema.define(version: 20180507131556) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -22,15 +22,6 @@ ActiveRecord::Schema.define(version: 20180507092852) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.index ["country_code", "code"], name: "index_activities_on_country_code_and_code"
-  end
-
-  create_table "codes", force: :cascade do |t|
-    t.string "category"
-    t.string "code"
-    t.string "language"
-    t.string "description"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
   end
 
   create_table "companies", force: :cascade do |t|
