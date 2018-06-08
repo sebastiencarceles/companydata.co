bbe8c5bc6a65a69f386aefa054c7d9b2a2987b72
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-06-08 22:50:48 +0200

    show customer portal

--- lib/proabono.rb
+++ lib/proabono.rb
@@ -9,26 +9,44 @@ class Proabono
   end
 
   def update
-    call("/Customer", ReferenceCustomer: @user.id, Email: @user.email)
+    post("/Customer", ReferenceCustomer: @user.id, Email: @user.email)
   end
 
-  def subscribe
-    call("/Subscription", ReferenceCustomer: @user.id, ReferenceOffer: "free_trial")
+  def customer_portal
+    update[:Links].select { |link| link[:rel] == "hosted-home" }.first[:href]
   end
 
   def usage
-    call("/Usage", ReferenceCustomer: @user.id, ReferenceFeature: "api_call", Increment: 1, DateStamp: DateTime.now)
+    post("/Usage", ReferenceCustomer: @user.id, ReferenceFeature: "api_call", Increment: 1, DateStamp: DateTime.now)
+  end
+
+  def subscribe
+    post("/Subscription", ReferenceCustomer: @user.id, ReferenceOffer: "free_trial")
+  end
+
+  def subscription
+    get("/Subscription", ReferenceCustomer: @user.id)
+  end
+
+  def subscription_name
+    subscription&.dig(:TitleLocalized)
   end
 
   private
 
-    def call(path, body)
-      self.class.post(path, body: body, basic_auth: auth,  headers: { 
-        "Accept" => "application/json" 
-      })
+    def get(path, query)
+      self.class.get(path, query: query, basic_auth: auth, headers: headers).parsed_response&.with_indifferent_access
+    end
+
+    def post(path, body)
+      self.class.post(path, body: body, basic_auth: auth,  headers: headers).parsed_response&.with_indifferent_access
     end
 
     def auth
       @auth ||= { username: Figaro.env.PROABONO_USERNAME, password: Figaro.env.PROABONO_PASSWORD }
+    end
+
+    def headers
+      { Accept: "application/json" }
     end
 end
