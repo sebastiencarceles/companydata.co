2a4cbc642a00640c9f79ab6ca5c65bd4972e3a79
Sébastien Carceles <sebastien.carceles@gmail.com>
2018-06-24 09:00:11 +0200

    remove tracking

--- app/controllers/api/v1/unauth_companies_controller.rb
+++ app/controllers/api/v1/unauth_companies_controller.rb
@@ -1,8 +1,6 @@
 # frozen_string_literal: true
 
 class Api::V1::UnauthCompaniesController < ActionController::API
-  after_action :track_api_call
-
   def autocomplete
     query = params[:q]
     render(json: {}, status: :bad_request) && (return) unless query
@@ -18,10 +16,4 @@ class Api::V1::UnauthCompaniesController < ActionController::API
     )
     render json: results, each_serializer: Api::V1::LigthCompanySerializer
   end
-
-  private
-
-    def track_api_call
-      Tracking::TrackWorker.perform_async("unauthenticated_user", "Unauthenticated API call")
-    end
 end
