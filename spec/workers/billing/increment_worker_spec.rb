a142aaee28ec222c7a6a7cbee4f269d8ed17a063
Sebastien Carceles <sebastien.carceles@gmail.com>
2018-06-27 21:45:55 +0200

    temporarily unplug email notifications

--- spec/workers/billing/increment_worker_spec.rb
+++ spec/workers/billing/increment_worker_spec.rb
@@ -13,22 +13,22 @@ RSpec.describe Billing::IncrementWorker, type: :worker do
     expect(proabono).to receive(:increment_by).with(counter_value).and_return(Code: nil)
     subject
   end
+  # TODO temporarily unplug email notifications
+  # it "sends emails when the user has no active subscription" do
+  #   allow(proabono).to receive(:increment_by).with(counter_value).and_return(Code: "Error.Api.Usage.NoneMatching")
+  #   expect { subject }.to change { Sidekiq::Worker.jobs.size }.by(1)
+  #   expect(Sidekiq::Worker.jobs.last["args"][0]["arguments"]).to eq ["UserMailer", "no_subscription", "deliver_now", { "user_id" => user.id, "_aj_symbol_keys" => ["user_id"] }]
+  # end
 
-  it "sends emails when the user has no active subscription" do
-    allow(proabono).to receive(:increment_by).with(counter_value).and_return(Code: "Error.Api.Usage.NoneMatching")
-    expect { subject }.to change { Sidekiq::Worker.jobs.size }.by(1)
-    expect(Sidekiq::Worker.jobs.last["args"][0]["arguments"]).to eq ["UserMailer", "no_subscription", "deliver_now", { "user_id" => user.id, "_aj_symbol_keys" => ["user_id"] }]
-  end
+  # it "sends emails when the user has no paying method" do
+  #   allow(proabono).to receive(:increment_by).with(counter_value).and_return(Code: "Error.Customer.PaymentSettings.Missing")
+  #   expect { subject }.to change { Sidekiq::Worker.jobs.size }.by(1)
+  #   expect(Sidekiq::Worker.jobs.last["args"][0]["arguments"]).to eq ["UserMailer", "missing_payment_method", "deliver_now", { "user_id" => user.id, "_aj_symbol_keys" => ["user_id"] }]
+  # end
 
-  it "sends emails when the user has no paying method" do
-    allow(proabono).to receive(:increment_by).with(counter_value).and_return(Code: "Error.Customer.PaymentSettings.Missing")
-    expect { subject }.to change { Sidekiq::Worker.jobs.size }.by(1)
-    expect(Sidekiq::Worker.jobs.last["args"][0]["arguments"]).to eq ["UserMailer", "missing_payment_method", "deliver_now", { "user_id" => user.id, "_aj_symbol_keys" => ["user_id"] }]
-  end
-
-  it "sends emails when the user has unpaid invoices" do
-    allow(proabono).to receive(:increment_by).with(counter_value).and_return(Code: "Error.Customer.Billing.CappingReached")
-    expect { subject }.to change { Sidekiq::Worker.jobs.size }.by(1)
-    expect(Sidekiq::Worker.jobs.last["args"][0]["arguments"]).to eq ["UserMailer", "unpaid_invoices", "deliver_now", { "user_id" => user.id, "_aj_symbol_keys" => ["user_id"] }]
-  end
+  # it "sends emails when the user has unpaid invoices" do
+  #   allow(proabono).to receive(:increment_by).with(counter_value).and_return(Code: "Error.Customer.Billing.CappingReached")
+  #   expect { subject }.to change { Sidekiq::Worker.jobs.size }.by(1)
+  #   expect(Sidekiq::Worker.jobs.last["args"][0]["arguments"]).to eq ["UserMailer", "unpaid_invoices", "deliver_now", { "user_id" => user.id, "_aj_symbol_keys" => ["user_id"] }]
+  # end
 end
